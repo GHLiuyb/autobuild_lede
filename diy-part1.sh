b3482b5b470904f158a73cfa4626071ebfaf93c5
GHLiuyb <86706862+GHLiuyb@users.noreply.github.com>
2021-07-06 11:20:13 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -18,3 +18,25 @@
 
 git clone https://github.com/liuybgh/OpenAppFilter package/OpenAppFilter
 git clone https://github.com/liuybgh/luci-app-koolproxyR package/luci-app-koolproxyR
+git clone https://github.com/openwrt/luci/tree/master/applications package/
+git clone https://github.com/immortalwrt/luci-app-unblockneteasemusic package/luci-app-unblockmusic
+git clone https://github.com/pymumu/luci-app-smartdns package/luci-app-smartdns
+git clone https://github.com/kuoruan/luci-app-kcptun package/luci-app-kcptun
+git clone https://github.com/jerrykuku/luci-theme-argon package/luci-theme-argon
+git clone https://github.com/lisaac/luci-app-dockerman package/luci-app-dockerman
+git clone https://github.com/shadowsocks/luci-app-shadowsocks package/luci-app-shadowsocks
+git clone https://github.com/rufengsuixing/luci-app-adguardhome package/luci-app-adguardhome
+git clone https://github.com/frainzy1477/luci-app-clash package/luci-app-clash
+git clone https://github.com/kuoruan/luci-app-v2ray package/luci-app-v2ray
+git clone https://github.com/maxlicheng/luci-app-ssr-plus package/luci-app-ssr-plus
+git clone https://github.com/jerrykuku/luci-app-jd-dailybonus package/luci-app-jd-dailybonus
+git clone https://github.com/LuttyYang/luci-theme-material package/luci-theme-material
+git clone https://github.com/AlexZhuo/luci-app-shadowsocksR package/luci-app-shadowsocksR
+git clone https://github.com/kuoruan/luci-app-frpc package/luci-app-frpc
+git clone https://github.com/honwen/luci-app-aliddns package/luci-app-aliddns
+git clone https://github.com/lisaac/luci-app-diskman package/luci-app-diskman
+#git clone  package/
+#git clone  package/
+#git clone  package/
+#git clone  package/
+
