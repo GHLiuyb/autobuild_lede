b02efffe6210c1e24b7a5848f77f56d70ef233b4
GHLiuyb <86706862+GHLiuyb@users.noreply.github.com>
2022-05-25 09:25:13 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,6 +12,6 @@
 
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
-sed -i 's/luci-theme-bootstrap/luci-theme-argon/' feeds/luci/collections/luci/Makefile
-sed -i 's/192.168.1.1/10.10.10.251/g' package/base-files/files/bin/config_generate
-sed -i 's/192.168./10.10./g' package/base-files/files/bin/config_generate
+#sed -i 's/luci-theme-bootstrap/luci-theme-argon/' feeds/luci/collections/luci/Makefile
+#sed -i 's/192.168.1.1/10.10.10.251/g' package/base-files/files/bin/config_generate
+#sed -i 's/192.168./10.10./g' package/base-files/files/bin/config_generate
